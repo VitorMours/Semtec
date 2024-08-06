feae9a89d159f3018c8288a1dfac1b03b5e39915
Vitor Moura <jvrezendemoura@gmail.com>
2024-08-06 10:14:26 -0300

    Adicionando primeiros estilos, modularizando os componentes, e adicionando as páginas de erro

--- src/views.py
+++ src/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, render_template 
+from flask import Blueprint, redirect, render_template, request, url_for 
 
 
 
@@ -11,3 +11,28 @@ views = Blueprint("views", __name__, template_folder="templates", static_folder=
 @views.route('/')
 def index():
     return render_template("index.jinja")
+
+
+@views.route('/register', methods=["GET","POST"])
+def register():
+
+    if request.method == "GET":
+        return render_template("register.jinja")
+
+    elif request.method == "POST":
+        return "Registrando elementos"
+
+    else:
+        return redirect(url_for("views.error"))
+        
+
+
+
+
+@views.app_errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
+
+@views.app_errorhandler(405)
+def invalid_method(error):
+    return render_template("405.html"), 405
